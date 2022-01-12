be3fb1eb456ed8057bc9ac62fc1420395895a464
Dmitrii Veretennikov <dima_veretennikov@list.ru>
2022-01-12 21:10:42 +0400

    update package.json

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import express from 'express'
-import typeDefs from './src/schema/index.js'
-import path, {dirname} from 'path'
+import express from 'express';
+import typeDefs from './src/schema/index.js';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { resolvers } from './src/resolvers/index.js'
-import cors from 'cors'
+import { resolvers } from './src/resolvers/index.js';
+import cors from 'cors';
 
-import { ApolloServer } from 'apollo-server-express'
-import { Vacancies } from './src/api/Vacancies.js'
+import { ApolloServer } from 'apollo-server-express';
+import { Vacancies } from './src/api/Vacancies.js';
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   const server = new ApolloServer({
@@ -16,9 +16,9 @@ const startServer = async () => {
     resolvers,
     dataSources: () => {
       return {
-        api: new Vacancies()
-      }
-    }
+        api: new Vacancies(),
+      };
+    },
   });
 
   await server.start();
@@ -27,9 +27,8 @@ const startServer = async () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
 
-  app.use(cors())
-  app.use(express.static(__dirname + '/client/build'))
-
+  app.use(cors());
+  app.use(express.static(__dirname + '/client/build'));
 
   server.applyMiddleware({ app });
 
@@ -39,4 +38,3 @@ const startServer = async () => {
 };
 
 const { app, server } = startServer();
-
